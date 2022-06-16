4c776a7d341dbdbe0782375845620ddfcff7a596
AnnaSems <sem.mm@mail.ru>
2022-06-16 11:59:00 +1000

    add update user

--- to_do_project/user/views.py
+++ to_do_project/user/views.py
@@ -1,21 +1,21 @@
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import User
 from .serializer import UserModelSerializer
 
 
-class UserModelViewSet(ViewSet):
+class UserModelViewSet(ModelViewSet):
+    serializer_class = UserModelSerializer
+    queryset = User.objects.all()
+
     def list(self, request):
-        users = User.objects.all()
-        serializer = UserModelSerializer(users, many=True)
+        queryset = User.objects.all()
+        serializer = UserModelSerializer(queryset, many=True)
         return Response(serializer.data)
 
-    def retrieve(self, request, pk=None):
+    def retrieve(self, request, pk=None, ):
         queryset = User.objects.all()
         user = get_object_or_404(queryset, pk=pk)
         serializer = UserModelSerializer(user)
         return Response(serializer.data)
-
-    def update(self, request, *args, **kwargs):
-        pass
